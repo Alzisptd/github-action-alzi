9fb866a5541982f793033d693013f3f92fcadf90
Alzi <aljisaptiadi@gmail.com>
2024-12-30 16:04:46 +0700

    Local to GCP Update 7

--- mongo/src/index.ts
+++ mongo/src/index.ts
@@ -3,6 +3,7 @@ import db from "./utils/database";
 import routes from "./routes/api";
 import bodyParser from "body-parser";
 import docs from "./docs/route";
+import cors from 'cors';
 
 
 const PORT = 5000;
@@ -12,6 +13,13 @@ async function init() {
     await db();
 
     const app = express();
+
+    // Konfigurasi CORS
+    app.use(cors({
+      origin: 'http://34.31.192.61:3000', // URL frontend Anda
+      methods: ['GET', 'POST', 'PUT', 'DELETE'], // Sesuaikan metode yang diizinkan
+      allowedHeaders: ['Content-Type', 'Authorization'], // Header yang diperbolehkan
+    }));
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
